766bc342d5c278409964b0080b6ea549f5d83796
Mamun <mamun511510782@gmail.com>
2020-12-26 10:00:06 +0600

    Introduction of OOP

---
+++ 01first.php
@@ -0,0 +1,30 @@
+<?php
+    class calculation{
+        public $a, $b, $c;
+
+        function sum(){
+            $this->c = $this->a + $this->b;
+
+            return $this->c;
+        }
+        function sub(){
+            $this->c = $this->a - $this->b;
+
+            return $this->c;
+        }
+    }
+
+    $c1 = new calculation();
+
+    $c1->a = 20;
+    $c1->b = 30;
+
+    $c2 = new calculation();
+
+    $c2->a = 50;
+    $c2->b = 20;
+
+    echo "Sum value of C1 : ".$c1->sum() ."<br>";
+
+    echo "Sub value of C2".$c2->sub();
+?>
