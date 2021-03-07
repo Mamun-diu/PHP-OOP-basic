1cebc84a05e0eb26b5d139ec114924f4adc23140
Mamun <mamun511510782@gmail.com>
2021-03-07 12:37:45 +0600

    Method Chaining

---
+++ 13index.php
@@ -0,0 +1,24 @@
+<!-- Method Chaining -->
+<?php
+    class abc{
+        public function first(){
+            echo "This is first function <br>";
+            return $this;
+        }
+        public function second(){
+            echo "This is second function <br>";
+            return $this;
+        }
+        public function third(){
+            echo "This is third function <br>";
+        }
+    }
+
+    $test = new abc;
+    $test->first();
+    $test->second();
+    $test->third();
+
+    echo "<br>";
+    $test->first()->second()->third();
+?>
